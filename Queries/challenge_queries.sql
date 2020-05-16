8855a18a22b5ab006ba941f7ffa6a44df7330552
Rob West <songmanrob@gmail.com>
2020-05-16 13:21:34 -0500

    adding deliverable 2 and updating challenge queries

--- Queries/challenge_queries.sql
+++ Queries/challenge_queries.sql
@@ -54,3 +54,28 @@ order by tt.title,
     ce.last_name,
 	ce.first_name,
 	s.salary;
+
+-- Deliverable #2
+-- Create table of mentorship eligibility
+-- Include emp_no, last_name, first_name, title, from_date, to_date
+-- Only current employees born in 1965
+select e.emp_no,
+    e.last_name,
+    e.first_name,
+    tnd.title,
+    tnd.from_date,
+    tnd.to_date
+into mentorship_eligibility
+from employees as e
+inner join titles_no_dups as tnd
+on (e.emp_no = tnd.emp_no)
+inner join dept_emp as de
+on (tnd.emp_no = de.emp_no)
+where (e.birth_date between '1965-01-01' and '1965-12-31')
+and (de.to_date = '9999-01-01')
+order by tnd.title,
+    e.last_name,
+    e.first_name,
+    tnd.to_date,
+    tnd.from_date,
+    e.emp_no;
